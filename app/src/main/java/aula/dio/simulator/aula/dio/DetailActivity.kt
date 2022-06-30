de33d5ac38c09ebb1679828c425350f66c338d79
Denis Tomás <denistomas03@gmail.com>
2022-06-30 11:31:27 -0300

    Fixed my mistakes after watching the classes again.

--- app/src/main/java/aula/dio/simulator/aula/dio/DetailActivity.kt
+++ app/src/main/java/aula/dio/simulator/aula/dio/DetailActivity.kt
@@ -1,12 +1,13 @@
 package aula.dio.simulator.aula.dio
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import aula.dio.simulator.aula.dio.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+
 
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
